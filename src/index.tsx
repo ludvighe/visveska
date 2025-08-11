b5bdac3449b0ad64236067044b0f5411e4c74e4f
lu <ludvigfh@pm.me>
2025-08-11 22:24:12 +0200

    Simplify a little

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-/* @refresh reload */
-import { render } from "solid-js/web";
-
-import "./index.scss";
-import PageRouter from "./routes";
-
-render(() => <PageRouter />, document.getElementById("root") as HTMLElement);
